6cce34dad8509c3a88b9a6a3b772e496b9eebbcd
nongnong10 <jirachi@gmail.com>
2023-03-08 11:42:12 +0700

    Update font and icon

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const taskInput = document.querySelector(".task-input");
 const taskButton = document.querySelector(".submit-btn");
 const taskList = document.querySelector(".task-list");
-
+const taskStatus = document.querySelector(".task-status");
 //Event listener
+document.addEventListener("DOMContentLoaded", getTask);
 taskButton.addEventListener("click", addTask);
 taskList.addEventListener("click", deleteTask);
+taskStatus.addEventListener("click", filterTodo);
 
 //Fuctions
 function addTask(event){
@@ -21,15 +23,18 @@ function addTask(event){
     newTask.classList.add('task-item');
     taskDiv.appendChild(newTask);
 
+    //Add todo to localstorage
+    saveLocalTask(taskInput.value);
+
     //Create Delete Button
     const deleteButton = document.createElement('button');
-    deleteButton.innerText = 'Delete';
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
     deleteButton.classList.add("delete-btn");
     taskDiv.appendChild(deleteButton);
 
     //Create Done Button
     const doneButton = document.createElement('button');
-    doneButton.innerHTML = 'Done';
+    doneButton.innerHTML = '<i class="fa-solid fa-check"></i>';
     doneButton.classList.add('done-btn');
     taskDiv.appendChild(doneButton);
 
@@ -44,6 +49,7 @@ function deleteTask(event){
         const todo = item.parentElement;
         todo.classList.add('fall');
         // todo.remove();
+        removeLocalTask(todo);
         todo.addEventListener("transitionend", function(){
             todo.remove();
         });
@@ -51,4 +57,91 @@ function deleteTask(event){
     if (item.classList[0] == 'done-btn'){
         item.parentElement.classList.toggle('done');
     }
+}
+
+function filterTodo(event){
+    const todos = taskList.childNodes;
+    console.log(todos);
+    todos.forEach(function(task){
+        switch(event.target.value){
+            case "all": 
+            task.style.display = "flex";
+                break;
+            case "done":
+                console.log("done");
+                if (task.classList.contains("done")) {
+                    task.style.display = "flex";
+                } else {
+                    task.style.display = "none";
+                }
+                break;
+            case "doing":
+                if (!task.classList.contains("done")) {
+                    task.style.display = "flex";
+                } else {
+                    task.style.display = "none";
+                }
+                break;
+        }
+    });
+}
+
+function saveLocalTask(task){
+    let todos;
+    if (localStorage.getItem("todos") === null){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.push(task);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getTask(){
+    let todos;
+    if (localStorage.getItem("todos") === null){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.forEach(function(todo){
+        //Task div
+        const taskDiv = document.createElement("div");
+        taskDiv.classList.add("task"); 
+
+        //Create task li
+        const newTask = document.createElement("li");
+        newTask.innerText = todo;
+        newTask.classList.add('task-item');
+        taskDiv.appendChild(newTask);
+
+        //Create Delete Button
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+        deleteButton.classList.add("delete-btn");
+        taskDiv.appendChild(deleteButton);
+
+        //Create Done Button
+        const doneButton = document.createElement('button');
+        doneButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+        doneButton.classList.add('done-btn');
+        taskDiv.appendChild(doneButton);
+
+        console.log(1);
+        taskList.appendChild(taskDiv);
+    });
+}
+
+function removeLocalTask(todo){
+    let todos;
+    if (localStorage.getItem("todos") === null){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    
+    
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
